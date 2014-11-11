77c767c8d27968afc6c165f5c9ccc35da089dd6c
Nicolas Jeker <n.jeker@gmx.net>
2014-11-11 10:09:13 +0100

    make colors script work as expected

--- init/10_colors.sh
+++ init/10_colors.sh
@@ -1,4 +1,17 @@
 color=base16-monokai.dark.256.xresources
-echo "Setting urxvt color to $color"
 colordir=$(realpath "${HOME}/.colors")
-ln -sv "$colordir/base16-xresources/$color" "$colordir/current" 
+e_header "Setting urxvt color to $color"
+skip="$("link_test" "$colordir/current" "$colordir/base16-xresources/$color")"
+if [[ "$skip" ]]; then
+  e_error "Skipping ~/$base, $skip."
+  return 1;
+fi 
+if [[ -e "$colordir/current" ]] ; then
+    # Destination file already exists in ~/.colors Back it up!
+    e_arrow "Backing up $colordir/current"
+    # create backup directory when it doesn't exist already
+    [[ -e "$backup_dir" ]] || mkdir -p "$backup_dir"
+    mv "$colordir/current" "$backup_dir"
+    backup=1
+fi
+ln -sv "$colordir/base16-xresources/$color" "$colordir/current"
