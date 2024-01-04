0afee618fefe52ab3b9ce259b84185c3b0de2080
Nicolas Jeker <n.jeker@gmx.net>
2024-01-04 21:11:45 +0100

    fish: Handle multiple arguments in mkcd function

--- config/fish/functions/mkcd.fish
+++ config/fish/functions/mkcd.fish
@@ -1,3 +1,12 @@
-function mkcd
-mkdir $args && cd $args
+function mkcd -d "Create a directory and set CWD"
+    command mkdir $argv
+    if test $status = 0
+        switch $argv[(count $argv)]
+            case '-*'
+
+            case '*'
+                cd $argv[(count $argv)]
+                return
+        end
+    end
 end
